e9f45321890d8440315fdc0c5ac50d4fb5c9add4
agus <smartgdi@gmail.com>
2013-06-28 21:43:45 +0700

    Modify make_.sh

--- make_.sh
+++ make_.sh
@@ -1,4 +1,4 @@
 # !/bin/sh
 #set -x
 
-valac -X -lm --pkg gtk+-3.0 --pkg sqlite3 src/pray_time.vala src/minbar.vala src/calendar.vala src/preference.vala src/utils.vala -o adzan_alarm
+valac -X -lm --pkg gtk+-3.0 --pkg sqlite3 src/pray_time.vala src/adzan_alarm.vala src/calendar.vala src/preference.vala src/utils.vala -o adzan_alarm
